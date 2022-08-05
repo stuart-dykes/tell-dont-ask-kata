870b5781dac1cd39ed9ec506dff982062cb6f5ab
Stuart Dykes <stuart.dykes@fanduel.com>
2022-08-05 11:24:58 +0100

    Use record

--- java/src/main/java/it/gabrieletondi/telldontaskkata/domain/Category.java
+++ java/src/main/java/it/gabrieletondi/telldontaskkata/domain/Category.java
@@ -2,15 +2,7 @@ package it.gabrieletondi.telldontaskkata.domain;
 
 import java.math.BigDecimal;
 
-public class Category {
-
-	private final String name;
-	private final BigDecimal taxPercentage;
-
-	public Category( final String name, final BigDecimal taxPercentage ) {
-		this.name = name;
-		this.taxPercentage = taxPercentage;
-	}
+public record Category(String name, BigDecimal taxPercentage) {
 
 	public String getName() {
 		return name;
