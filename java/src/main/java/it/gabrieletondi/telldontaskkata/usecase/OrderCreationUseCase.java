a905bb83217cb613b8c6ec10dc4b448ccc0a875b
Stuart Dykes <stuart.dykes@fanduel.com>
2022-08-05 10:36:44 +0100

    getTaxedAmount

--- java/src/main/java/it/gabrieletondi/telldontaskkata/usecase/OrderCreationUseCase.java
+++ java/src/main/java/it/gabrieletondi/telldontaskkata/usecase/OrderCreationUseCase.java
@@ -13,52 +13,54 @@ import it.gabrieletondi.telldontaskkata.repository.OrderRepository;
 import it.gabrieletondi.telldontaskkata.repository.ProductCatalog;
 
 public class OrderCreationUseCase {
-    private final OrderRepository orderRepository;
-    private final ProductCatalog productCatalog;
+	private final OrderRepository orderRepository;
+	private final ProductCatalog productCatalog;
 
-    public OrderCreationUseCase(OrderRepository orderRepository, ProductCatalog productCatalog) {
-        this.orderRepository = orderRepository;
-        this.productCatalog = productCatalog;
-    }
+	public OrderCreationUseCase( OrderRepository orderRepository, ProductCatalog productCatalog ) {
+		this.orderRepository = orderRepository;
+		this.productCatalog = productCatalog;
+	}
 
-    public void run(SellItemsRequest request) {
-        Order order = new Order();
-        order.setStatus(OrderStatus.CREATED);
-        order.setItems(new ArrayList<>());
-        order.setCurrency("EUR");
-        order.setTotal(new BigDecimal("0.00"));
-        order.setTax(new BigDecimal("0.00"));
+	public void run( SellItemsRequest request ) {
+		Order order = new Order();
+		order.setStatus( OrderStatus.CREATED );
+		order.setItems( new ArrayList<>() );
+		order.setCurrency( "EUR" );
+		order.setTotal( new BigDecimal( "0.00" ) );
+		order.setTax( new BigDecimal( "0.00" ) );
 
-        for (SellItemRequest itemRequest : request.getRequests()) {
-            Product product = productCatalog.getByName(itemRequest.getProductName());
+		for ( SellItemRequest itemRequest : request.getRequests() ) {
+			Product product = productCatalog.getByName( itemRequest.getProductName() );
 
-            if (product == null) {
-                throw new UnknownProductException();
-            }
-            else {
-                final BigDecimal taxAmount = getTaxAmount( product, itemRequest.getQuantity() );
+			if ( product == null ) {
+				throw new UnknownProductException();
+			} else {
+				final BigDecimal taxAmount = getTaxAmount( product, itemRequest.getQuantity() );
 
-                final BigDecimal unitaryTaxedAmount = product.getUnitaryTaxedAmount();
-                final BigDecimal taxedAmount = unitaryTaxedAmount.multiply(
-                        BigDecimal.valueOf( itemRequest.getQuantity() ) ).setScale( 2, HALF_UP );
+				final BigDecimal taxedAmount = getTaxedAmount( itemRequest, product );
 
-                final OrderItem orderItem = new OrderItem();
-                orderItem.setProduct( product );
-                orderItem.setQuantity( itemRequest.getQuantity() );
-                orderItem.setTax( taxAmount );
-                orderItem.setTaxedAmount( taxedAmount );
+				final OrderItem orderItem = new OrderItem();
+				orderItem.setProduct( product );
+				orderItem.setQuantity( itemRequest.getQuantity() );
+				orderItem.setTax( taxAmount );
+				orderItem.setTaxedAmount( taxedAmount );
 
-                order.addItem( orderItem );
-            }
-        }
+				order.addItem( orderItem );
+			}
+		}
 
-        orderRepository.save( order );
-    }
+		orderRepository.save( order );
+	}
 
-    private BigDecimal getTaxAmount( final Product product, final int quantity ) {
-        final BigDecimal unitaryTax = product.getUnitaryTax();
-        final BigDecimal taxAmount = unitaryTax.multiply( BigDecimal.valueOf( quantity ) );
-        return taxAmount;
-    }
+	private BigDecimal getTaxedAmount( final SellItemRequest itemRequest, final Product product ) {
+		final BigDecimal unitaryTaxedAmount = product.getUnitaryTaxedAmount();
+		return unitaryTaxedAmount.multiply( BigDecimal.valueOf( itemRequest.getQuantity() ) )
+				.setScale( 2, HALF_UP );
+	}
+
+	private BigDecimal getTaxAmount( final Product product, final int quantity ) {
+		final BigDecimal unitaryTax = product.getUnitaryTax();
+		return unitaryTax.multiply( BigDecimal.valueOf( quantity ) );
+	}
 
 }
